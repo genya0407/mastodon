f13703234ea9ceda0aea14dcfb5a4942a737b161
Yusuke Sangenya <longinus.eva@gmail.com>
2024-02-19 22:44:45 +0900

    Merge pull request #7 from genya0407/feature/emoji-2
    Show emoji on accounts API & place emojis in paragraph

--- app/serializers/rest/reacted_status_serializer.rb
+++ app/serializers/rest/reacted_status_serializer.rb
@@ -7,13 +7,15 @@ class REST::ReactedStatusSerializer < REST::StatusSerializer
 
     parsed_original_content = Nokogiri::HTML::DocumentFragment.parse(original_content)
     Nokogiri::HTML::Builder.with(parsed_original_content) do |doc|
-      object.emoji_count.each do |emoji, count|
-        doc.span do
-          if emoji.start_with?('http')
-            doc.img(height: '30').src = emoji
-            doc.span(" (#{count})")
-          else
-            doc.span("#{emoji} (#{count})")
+      doc.p do
+        object.emoji_count.each do |emoji, count|
+          doc.span do
+            if emoji.start_with?('http')
+              doc.img(height: '30').src = emoji
+              doc.span(" (#{count})")
+            else
+              doc.span("#{emoji} (#{count})")
+            end
           end
         end
       end
