1fef6b78fc5f4828279fe1ac9bb04458b1a72629
Yusuke Sangenya <longinus.eva@gmail.com>
2024-02-21 22:50:48 +0900

    Show custom emoji

--- app/serializers/rest/notification_serializer.rb
+++ app/serializers/rest/notification_serializer.rb
@@ -4,7 +4,7 @@ class REST::NotificationSerializer < ActiveModel::Serializer
   attributes :id, :type, :created_at
 
   belongs_to :from_account, key: :account, serializer: REST::AccountSerializer
-  belongs_to :target_status, key: :status, if: :status_type?, serializer: REST::StatusSerializer
+  belongs_to :target_status, key: :status, if: :status_type?, serializer: REST::ReactedStatusSerializer
   belongs_to :report, if: :report_type?, serializer: REST::ReportSerializer
 
   def id
