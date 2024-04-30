0e2ea8004c4b17d4d36fca8dc030788a8459c659
Yusuke Sangenya <longinus.eva@gmail.com>
2024-05-01 00:40:04 +0900

    Show reaction as emoji

--- app/serializers/rest/reacted_status_serializer.rb
+++ app/serializers/rest/reacted_status_serializer.rb
@@ -8,20 +8,10 @@ class REST::ReactedStatusSerializer < REST::StatusSerializer
     parsed_original_content = Nokogiri::HTML::DocumentFragment.parse(original_content)
     Nokogiri::HTML::Builder.with(parsed_original_content) do |doc|
       doc.p do
-        object.emoji_count.each do |emoji, count|
+        object.emoji_count.each.with_index do |(emoji, count), i|
           doc.span do
             if emoji.start_with?('http')
-              doc.img(height: '30').src = Rails.cache.fetch("reacted_status_serializer:image:#{Digest::MD5.hexdigest(emoji)}", expires_in: 24.hours) do
-                base64 =
-                  begin
-                    Base64.strict_encode64(HTTP.timeout(3).follow.get(emoji).body)
-                  rescue HTTP::TimeoutError => e
-                    Rails.logger.warn("Failed to fetch #{emoji.inspect}: #{e}")
-                    nil
-                  end
-                "data:image/png;base64,#{base64}"
-              end
-              doc.span(" (#{count})")
+              doc.span(":__emoji_reaction_#{i}: (#{count})")
             else
               doc.span("#{emoji} (#{count})")
             end
@@ -30,5 +20,20 @@ class REST::ReactedStatusSerializer < REST::StatusSerializer
       end
     end
     parsed_original_content.to_html
+  end
+
+  def to_h
+    super.tap do |hash|
+      hash[:emojis] += object.emoji_count.map.with_index do |(emoji, _), i|
+        next unless emoji.start_with?('http')
+
+        {
+          shortcode: "__emoji_reaction_#{i}",
+          url: emoji,
+          static_url: emoji,
+          visible_in_pick: true,
+        }
+      end.compact
+    end
   end
 end
