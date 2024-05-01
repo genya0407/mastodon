8b7a07160bd27dc83528872bd14d0657a6f8f661
Yusuke Sangenya <longinus.eva@gmail.com>
2024-05-01 20:07:23 +0900

    Show emoji reaction

--- app/serializers/rest/reacted_status_serializer.rb
+++ app/serializers/rest/reacted_status_serializer.rb
@@ -10,21 +10,7 @@ class REST::ReactedStatusSerializer < REST::StatusSerializer
       doc.p do
         object.emoji_count.each do |emoji, count|
           doc.span do
-            if emoji.start_with?('http')
-              doc.img(height: '30').src = Rails.cache.fetch("reacted_status_serializer:image:#{Digest::MD5.hexdigest(emoji)}", expires_in: 24.hours) do
-                base64 =
-                  begin
-                    Base64.strict_encode64(HTTP.timeout(3).follow.get(emoji).body)
-                  rescue HTTP::TimeoutError => e
-                    Rails.logger.warn("Failed to fetch #{emoji.inspect}: #{e}")
-                    nil
-                  end
-                "data:image/png;base64,#{base64}"
-              end
-              doc.span(" (#{count})")
-            else
-              doc.span("#{emoji} (#{count})")
-            end
+            doc.span("#{emoji} (#{count})")
           end
         end
       end
